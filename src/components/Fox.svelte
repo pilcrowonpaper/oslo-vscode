0d735931049866d18a72650546bdaeddff023b41
pilcrowOnPaper <80624252+pilcrowOnPaper@users.noreply.github.com>
2021-07-21 11:18:16 +0900

    Update Fox.svelte

--- src/components/Fox.svelte
+++ src/components/Fox.svelte
@@ -90,11 +90,5 @@ fill="{textActivity}" stroke="none">
     });
 
     function addBoop() {
-        if ($boopCount != "...") {
-            boopCount.set($boopCount + 1)
-            db.collection("pilcrow").doc("boopCount").update({
-                boops: firebase.firestore.FieldValue.increment(1)
-            });
-        }
     }
 </script>
